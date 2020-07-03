a692f202f62e679de7fd636a3ac354bdfb395515
jiadezhen <476024117@qq.com>
2020-07-03 21:33:29 +0800

    集成redis并实现验证码的获取及校验

---
+++ src/main/java/com/macro/mall/tiny/controller/UmsMemberController.java
@@ -0,0 +1,31 @@
+package com.macro.mall.tiny.controller;
+
+import com.macro.mall.tiny.common.api.CommonResult;
+import com.macro.mall.tiny.service.UmsMemberService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@Api(tags = "会员登录注册管理")
+@RestController
+@RequestMapping("/sso")
+public class UmsMemberController {
+
+    @Autowired
+    private UmsMemberService memberService;
+
+    @ApiOperation("获取验证码")
+    @RequestMapping(value = "/getAuthCode", method = RequestMethod.GET)
+    public CommonResult getAuthCode(@RequestParam String telephone) {
+        return memberService.generateAuthCode(telephone);
+    }
+
+    @ApiOperation("判断验证码是否正确")
+    @RequestMapping(value = "/verifyAuthCode", method = RequestMethod.POST)
+    public CommonResult updatePassword(@RequestParam String telephone,
+                                       @RequestParam String authCode) {
+        return memberService.verifyAuthCode(telephone,authCode);
+    }
+
+}
---
+++ src/main/java/com/macro/mall/tiny/service/impl/UmsMemberServiceImpl.java
@@ -0,0 +1,40 @@
+package com.macro.mall.tiny.service.impl;
+
+import com.macro.mall.tiny.common.api.CommonResult;
+import com.macro.mall.tiny.service.RedisService;
+import com.macro.mall.tiny.service.UmsMemberService;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+public class UmsMemberServiceImpl implements UmsMemberService {
+    @Autowired
+    private RedisService redisService;
+    @Value("${redis.key.prefix.authCode}")
+    private String REDIS_KEY_PREFIX_AUTH_CODE;
+    @Value("${redis.key.expire.authCode}")
+    private Long AUTH_CODE_EXPIRE_SECONDS;
+
+    @Override
+    public CommonResult generateAuthCode(String telephone) {
+        String authCode = RandomStringUtils.randomNumeric(6);
+        //验证码绑定手机号并存储到redis
+        redisService.set(REDIS_KEY_PREFIX_AUTH_CODE+telephone, authCode);
+        redisService.expire(REDIS_KEY_PREFIX_AUTH_CODE+telephone,AUTH_CODE_EXPIRE_SECONDS);
+        return CommonResult.success(authCode,"获取验证码成功");
+    }
+
+    @Override
+    public CommonResult verifyAuthCode(String telephone, String authCode) {
+        if(StringUtils.isEmpty(authCode)){
+            return CommonResult.failed("请输入验证码");
+        }
+        String realAuthCode = redisService.get(REDIS_KEY_PREFIX_AUTH_CODE + telephone);
+        if(authCode.equals(realAuthCode)){
+            return CommonResult.success(null,"验证码校验成功");
+        } else {
+            return CommonResult.failed("验证码不正确");
+        }
+    }
+}
