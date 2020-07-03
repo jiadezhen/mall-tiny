ed355fa8d1ebbc62bae7f14980495320ef873e4f
jiadezhen <476024117@qq.com>
2020-07-03 17:57:51 +0800

    springboot+mybatis基础骨架+GeneratorCode+swagger2

---
+++ src/main/java/com/macro/mall/tiny/MallTinyApplication.java
@@ -0,0 +1,15 @@
+package com.macro.mall.tiny;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.macro.mall.tiny.mbg.mapper")
+public class MallTinyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallTinyApplication.class, args);
+    }
+
+}
