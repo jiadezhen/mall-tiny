5b69c2d2eeb0ddb954c1dbce0b0160699ca9af34
jiadezhen <476024117@qq.com>
2020-07-05 23:28:09 +0800

    通用mapper1.3.7重新生成的层级文件

--- src/main/java/com/macro/mall/tiny/mbg/mapper/PmsBrandMapper.java
+++ src/main/java/com/macro/mall/tiny/mbg/mapper/PmsBrandMapper.java
@@ -2,11 +2,12 @@ package com.macro.mall.tiny.mbg.mapper;
 
 import com.macro.mall.tiny.mbg.model.PmsBrand;
 import com.macro.mall.tiny.mbg.model.PmsBrandExample;
-import java.util.List;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
+
 public interface PmsBrandMapper {
-    int countByExample(PmsBrandExample example);
+    long countByExample(PmsBrandExample example);
 
     int deleteByExample(PmsBrandExample example);
 
