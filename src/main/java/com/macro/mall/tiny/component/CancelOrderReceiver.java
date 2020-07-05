7e1ddbbea1290d58cab7810893db1f2ac33b80e6
jiadezhen <476024117@qq.com>
2020-07-05 12:40:54 +0800

    集成RabbitMQ-处理订单生成后超时则自动取消订单

---
+++ src/main/java/com/macro/mall/tiny/component/CancelOrderReceiver.java
@@ -0,0 +1,25 @@
+package com.macro.mall.tiny.component;
+
+import com.macro.mall.tiny.service.OmsPortalOrderService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * 取消订单消息的处理者
+ * Created by macro on 2018/9/14.
+ */
+@Slf4j
+@Component
+@RabbitListener(queues = "mall.order.cancel")
+public class CancelOrderReceiver {
+    @Autowired
+    private OmsPortalOrderService portalOrderService;
+    @RabbitHandler
+    public void handle(Long orderId){
+        log.info("receive delay message orderId:{}",orderId);
+        portalOrderService.cancelOrder(orderId);
+    }
+}
