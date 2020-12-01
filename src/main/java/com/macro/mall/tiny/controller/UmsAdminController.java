2037d320367247dfa13fc06ee4f35c7cf6f65cf9
jiadezhen <476024117@qq.com>
2020-12-01 10:04:57 +0800

    dev添加测试接口333333

--- src/main/java/com/macro/mall/tiny/controller/UmsAdminController.java
+++ src/main/java/com/macro/mall/tiny/controller/UmsAdminController.java
@@ -38,6 +38,7 @@ public class UmsAdminController {
         System.out.println("1111111111111111111111111111111111111111");
         System.out.println("1111111111111111111");
         System.out.println("222222222222222222222222222222222222");
+        System.out.println("33333333333333333");
         return CommonResult.success("testApi成功");
     }
 
